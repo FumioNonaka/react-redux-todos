d0179727ab35601c8e3187c5aa022e515c33fc1c
Fumio Nonaka <fumiononaka@FumioNonaka.local>
2019-08-16 23:01:34 +0900

    React + Redux入門 04: リスト項目のフィルタを加える

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import AddTodo from './AddTodo';
 import TodoList from './TodoList';
+import Footer from './Footer';
 import '../App.css';
 
 const App = () => (
 	<div>
 		<AddTodo />
 		<TodoList />
+		<Footer />
 	</div>
 );
 
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { toggleTodo } from '../actions';
+import { toggleTodo, VisibilityFilters } from '../actions';
 import Todo from './Todo';
 
 const TodoList = ({ todos, toggleTodo }) => (
@@ -15,8 +15,21 @@ const TodoList = ({ todos, toggleTodo }) => (
 	</ul>
 );
 
+const getVisibleTodos = (todos, filter) => {
+	switch (filter) {
+		case VisibilityFilters.SHOW_ALL:
+			return todos
+		case VisibilityFilters.SHOW_COMPLETED:
+			return todos.filter(t => t.completed)
+		case VisibilityFilters.SHOW_ACTIVE:
+			return todos.filter(t => !t.completed)
+		default:
+			throw new Error('Unknown filter: ' + filter)
+	}
+}
+
 const mapStateToProps = (state) => ({
-	todos: state.todos
+	todos: getVisibleTodos(state.todos, state.visibilityFilter)
 });
 
 const mapDispatchToProps = (dispatch) => ({
